d9883ba324598aa3534c520a887b3fff691627f9
Jake Cox <nicodangelo15@gmail.com>
2016-02-26 08:46:02 -0600

    yes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var amountFactor;
 var before;
 var speed;
 var interval;
-var fps;
+var ups;
 var interval;
 var moneyDisplay;
 var upgradeJobButton;
@@ -13,23 +13,51 @@ var jobButton;
 var currentJob;
 var shouldSave = true;
 var navIndex = 1;
+var width = 0;
+var loaded = false;
 
 var jobs = [
     /*0*/new Job("Candy Sales", 0.00, 0.25, null, true),
-    /*1*/new Job("Hot dog sales", 50.00, 1.00, null, true),
-    /*2*/new Job("Burger Flipper", 150.00, 2.75, null, true),
-    /*3*/new Job("Grocery Bagger", 270.65, 3.26, null, true),
-    /*4*/new Job("Laborer", 420.20, 5.50, null, true),
-    /*5*/new Job("Teacher", 690.00, 8.00, null, true),
-    /*6*/new Job("Dentist", 800.63, 10.00, null, true),
-    /*7*/new Job("Programmer", 1234.56, 12.75, null, true),
-    /*8*/new Job("Project Leader", 2580.69, 15.00, null, true),
-    /*9*/new Job("Manager", 5000.00, 20.00, null, true)
+    /*1*/new Job("Hot dog sales", 5.00, 1.00, null, true),
+    /*2*/new Job("Burger Flipper", 50.00, 2.75, null, true),
+    /*3*/new Job("Grocery Bagger", 70.65, 3.26, null, true),
+    /*4*/new Job("Laborer", 42.20, 5.50, null, true),
+    /*5*/new Job("Teacher", 90.00, 8.00, null, true),
+    /*6*/new Job("Dentist", 80.63, 10.00, null, true),
+    /*7*/new Job("Programmer", 34.56, 12.75, null, true),
+    /*8*/new Job("Project Leader", 280.69, 15.00, null, true),
+    /*9*/new Job("Manager", 500.00, 20.00, null, true),
+    /*10*/new Job("CEO", 1200.12, 33.33, null, true),
+    /*11*/new Job("Owner and Founder", 20000.00, 50.00, null, true),
+    /*12*/new Job("Mom", 2450.00, 69.00, null, true)
+    
+    /*actual costs: */
+    
+    /*0:  0.00*/
+    /*1:  50.00*/
+    /*2:  150.00*/
+    /*3:  270.65*/
+    /*4:  420.20*/
+    /*5:  690.00*/
+    /*6:  800.63*/
+    /*7:  1234.56*/
+    /*8:  2580.69*/
+    /*9:  5000.00*/
+    /*10: 1200.12*/
+    /*11: 20000.00*/
+    /*12: 245000.00*/
+    
 ];
+
+var companies = [
+	/*0*/new Company("Candy Store", 0, 1, 2.00, 0.25),
+	/*1*/new Company("Hot Dog Stand", 0, 1, 10.00, 1.00)
+];
+
+var progress = [0, 0];
 
 window.onload = function()
 {
-	"use strict";
     currentJob = 0;
     money = 0.0;
     if(shouldSave)
@@ -37,19 +65,30 @@ window.onload = function()
 	amountFactor = 0.25;
 	before = new Date().getTime();
 	speed = 0;
-	fps = 60;
-	interval = 1000/fps;
+	ups = 60;
+	interval = 1000/ups;
     moneyDisplay = document.getElementById('amount_display');
     upgradeJobButton = document.getElementById('upgrade_job_button');
     jobButton = document.getElementById('active_job_button'); 
     nav(1);
+	loaded = true;
+	
+	//initialize the companies
+	for(var i = 0; i < companies.length; i++)
+	{
+		$("#perchaseName" + i).text(companies[i].name);
+		$("#perchasePrice" + i).text("$" + (companies[i].price).toFixed(2));
+	}
 };
 
 function update(amountToUpdate)
 {
-    var newMoney = money.toFixed(2);
+	var newMoney = 0.0;
+	if(money != null)
+    	newMoney = money.toFixed(2);
     moneyDisplay.innerHTML = "$" + newMoney;
     updateJobs();
+	updateCompanies();
     if(shouldSave)
         save();
 }
@@ -57,15 +96,53 @@ function update(amountToUpdate)
 function updateJobs()
 {
     var newInc =  jobs[currentJob].inc.toFixed(2);
-    jobButton.innerHTML = jobs[currentJob].name + " +" + newInc;
+    jobButton.innerHTML = jobs[currentJob].name + " + $" + newInc;
     if(currentJob + 1 < jobs.length)
     {
         var tempPrice = jobs[currentJob + 1].price.toFixed(2); 
         if(currentJob > 0)
-            upgradeJobButton.innerHTML = "lv. " + currentJob + " Upgrade Job For " + tempPrice;
+            upgradeJobButton.innerHTML = "lv. " + currentJob + " Upgrade Job: $" + tempPrice;
         else
-            upgradeJobButton.innerHTML = " Upgrade Job For " + tempPrice;
+            upgradeJobButton.innerHTML = " Upgrade Job: $" + tempPrice;
     }
+}
+
+function updateCompanies()
+{
+	for(var i = 0; i < companies.length; i++)
+	{
+		//The owned Amount
+		$("#owned" + i).text(companies[i].owned);
+		
+		//The progress bar
+		if(companies[i].owned > 0)
+		{
+			
+			if(progress[i] > 99)
+				progress[i] = 0;
+			if(companies[i].owned > 29)
+			{
+				$("#bar"+ i).css("width",100+"%");
+			}
+			else
+			{
+				$("#bar"+ i).css("width",progress[i]+"%");
+			}
+			
+			progress[i] = progress[i] + companies[i].speed;
+			
+			if(progress[i] >= 100)
+			{
+				gainMoney(companies[i].inc);
+			}
+			
+			//the name and perchase amount
+			$("#owned" + i).text(companies[i].owned);
+			$("#name" + i).text(companies[i].name);
+			$("#cost" + i).text("$" + (companies[i].price).toFixed(2));
+			$("#inc" + i).text("$" + (companies[i].inc).toFixed(2));	
+		}
+	}
 }
 
 function clickme(index)
@@ -92,10 +169,27 @@ function clickmeNav(index)
     nav(index);
 }
 
+function perchaseCompany(index)
+{
+	if(money >= companies[index].price)
+	{
+		money = money - companies[index].price;
+		companies[index].owned += 1;
+		$("#notPerchased" + index).css("display", "none");
+		
+		if(companies[index].owned % 5 === 0)
+		{
+			companies[index].speed = companies[index].speed + 1;
+		}
+	}
+}
+
 function nav(index)
 {
-    if(index == navIndex)
+    if(index === navIndex)
+	{
         index = 1;
+	}
     for(var i = 1; i <= 8; i++)
     {
         $("#display" + i).css("display", "none");
@@ -107,18 +201,27 @@ function nav(index)
 //game loop
 setInterval(function(){
 	"use strict";
-	var now = new Date().getTime();
-	var elapsedTime = now - before;
-	if(elapsedTime > interval)
+	if(loaded)
 	{
-		update(Math.floor(elapsedTime/interval));
+		var now = new Date().getTime();
+		var elapsedTime = now - before;
+		if(elapsedTime > interval)
+		{
+			update(Math.floor(elapsedTime/interval));
+		}
+		else
+		{
+			update(1.0);
+		}
+		before = new Date().getTime();
 	}
-	else
-	{
-		update(1.0);
-	}
-	before = new Date().getTime();
 }, interval);
+
+//Takes in a number and makes it pretty
+function fixNumber()
+{
+	
+}
 
 //function to add money to the user(later will also keep track of the total earned and so on)
 function gainMoney(amount)
@@ -134,6 +237,16 @@ function Job(name, price, inc, inflation, current)
     this.inc = inc;
     this.inflation = inflation;
     this.current = current;
+}
+
+//The company Object
+function Company(name, owned, speed, price, inc)
+{
+	this.name = name;
+	this.owned = owned;
+	this.speed = speed;
+	this.price = price;
+	this.inc = inc;
 }
 
 function save()
